9410803fad3a4c32ae72c6c5716bc6a4f93d1474
Shahyad Sharghi <shahyads@yahoo.com>
2026-05-13 10:50:40 -0400

    support proxy, update node version, update axios, remove unused scopes

--- variableupdater/run-task.js
+++ variableupdater/run-task.js
@@ -1,4 +1,5 @@
 const tl = require("azure-pipelines-task-lib/task");
+const HttpsProxyAgent = require('https-proxy-agent');
 const axios = require("axios").default;
 
 async function main()
@@ -9,14 +10,26 @@ async function main()
         const token = tl.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false);
         const groupID = tl.getInput("VariableGroupId", true),
             varName = tl.getInput("VariableName", true),
-            newValue = tl.getInput("NewValue", true);
+            newValue = tl.getInput("NewValue", true),
+            useProxy = tl.getBoolInput("Proxy", false);
         
 		console.log(`VariableGroupId: ${groupID}`);
         console.log(`VariableName: ${varName}`);
+        console.log(`UseProxy: ${useProxy}`);
 
         const groupURL = `${process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}${process.env.SYSTEM_TEAMPROJECTID}/_apis/distributedtask/variablegroups/${groupID}?api-version=4.1-preview.1`;
         const axConf = {headers: {"Authorization": `Bearer ${token}`}};
-
+        if (useProxy)
+        {
+            const proxy = process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
+            console.log(`Proxy: ${proxy}`);
+            const httpsAgent = proxy ? new HttpsProxyAgent(proxy) : undefined;
+            if (httpsAgent)
+            {
+                axConf.proxy = false;
+                axConf.httpsAgent = httpsAgent;
+            }
+        }
         const group = (await axios.get(groupURL, axConf)).data,
             vars = group.variables
         const varEntry =  Object.entries(vars).find(([key]) => key.toLowerCase() == varName.toLowerCase());
